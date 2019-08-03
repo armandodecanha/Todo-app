29fa51c519fe4b44ed276474338b12b8e3bccf00
Armando de Canha <armandodecanha@gmail.com>
2019-08-04 00:35:07 +0100

    Ended app, conects now to the mongo database

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -7,41 +7,43 @@ mongoose.connect('mongodb+srv://armandodc:010495a@myment-zclbd.mongodb.net/test?
 // Create a schema - this is like a blueprint
 const todoSchema = new mongoose.Schema({
   item: String
-}) // What kind of information MongoDB is going to expect. This is going to take an object as a parameter,
-// and in this object we are kind of declare what our
-// to-do items are going to look like.
-// All those to-do items are just gonna have one property,
-// and that property name is going to be item
+})
 
 // Model
-const Todo = mongoose.model('Todo', todoSchema) // Todo is a model name wich is going to be stored as a
-// collection on MongoDB. The second parameter is the schema we are basing this model on.
-const itemOne = Todo({item: 'buy Flowers'}).save(function(err){ // We have created an item of type Todo. 
-//When we save Todo and pass in an object wich it expects, it then returns an object to us with a save method on it.
-  if (err) throw err;
-  console.log('item saved');
-})// We pass through the object that we want to provide wich is based on this schema.
+const Todo = mongoose.model('Todo', todoSchema);
 
-
-var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}]; // Dummy data
-const urlencodedParser = bodyParser.urlencoded({ extended: false }) // Middleware that we will run in the post request
+// var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}]; 
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 module.exports = function(app) {
 
 app.get('/todo', function(req, res) {
-    res.render('todo', {todos: data}); // data (array) is being passed through the view in this item (this property name)
+    // Get data from mongoDB and pass it to the view
+    Todo.find({}, function(err, data){ /* (has a callback function) find method can seach out to a collection and find either all the items in that collection or a particular
+        items in that collection. If we pass an empty object, we will get and retrieve all of the items in this Todo collection.
+        If you want an especified item, we can write {item: 'buy flowers'} and it will find it in that collection (if it exists). */
+        if (err) throw err;
+        res.render('todo', {todos: data}); /* When we sayender a view, we are passing this object {todos: data}
+        and we are using this 'data' variable, to pass through the view. Now we need to get the data from mongoDB itself.
+        How do we do that? Specify wich model or collection we want to get from. And the model is there: Todo. */
+    })
 });
 
 app.post('/todo', urlencodedParser, function(req, res) {
-    data.push(req.body) // Added this data to that array
-    res.json(data); // Send the data back to the front end
-});
+    // Get data from the view and add it to mongoDB
+    const newTodo = Todo(req.body).save(function(err, data){ // Creating a newTodo item, and we are going to get the item from req.body because there's where we are passing it to.
+        if (err) throw err;
+        res.json(data);
+    })
+});// updated data, back to the view
   
 app.delete('/todo/:item', function(req, res) {
-    data = data.filter(function(todo){ // Method called filter on the data (the array of objects)
-        return todo.item.replace(/ /g, '-') !== req.params.item; // Returns a true (item remains on the array) or false (item comes out of the array)
-    })
-    res.json(data); // Send the data back to the front end
+    // Delete the requested item from mongoDB
+    Todo.find({item: req.params.item.replace(/\-/g, "")}).deleteOne(function(err, data){/* Replacing the hyphens with a space,
+        and then we are going to look for these in the Todo collection, we're going to find the item.
+        We replace the hyphens with a space, because when it comes through as a URL, it has hyphen between the words, no spaces (omg <3)*/ 
+        if (err) throw err;
+        res.json(data); // Deletes it, then passes back the updated data.
+    });
 });
-
 };
